6132301c08857490cfc55d1202db49a4cd9c0b2b
davidsaltacc <d.lumpi@outlook.de>
2023-11-25 12:09:49 +0100

    don't save state for timer

--- src/main/java/net/shadowclient/main/module/modules/world/Timer.java
+++ src/main/java/net/shadowclient/main/module/modules/world/Timer.java
@@ -1,10 +1,12 @@
 package net.shadowclient.main.module.modules.world;
 
+import net.shadowclient.main.annotations.DontSaveState;
 import net.shadowclient.main.annotations.SearchTags;
 import net.shadowclient.main.module.Module;
 import net.shadowclient.main.module.ModuleCategory;
 import net.shadowclient.main.setting.settings.NumberSetting;
 
+@DontSaveState
 @SearchTags({"timer", "timer hack", "timerhack", "speedup", "speed up"})
 public class Timer extends Module {
 
