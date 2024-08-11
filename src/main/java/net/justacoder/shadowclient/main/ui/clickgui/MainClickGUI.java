843adebdb86e6ef19b999fdf10cc05d2c74ddd39
davidsaltacc <d.lumpi@outlook.de>
2024-08-11 21:13:20 +0200

    ye

--- src/main/java/net/justacoder/shadowclient/main/ui/clickgui/MainClickGUI.java
+++ src/main/java/net/justacoder/shadowclient/main/ui/clickgui/MainClickGUI.java
@@ -50,7 +50,7 @@ public class MainClickGUI extends ClickGUI {
             return;
         }
 
-        int screenWidth = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor()).width() / mc.options.getGuiScale().getValue();
+        int screenWidth = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor()).width() / (mc.options.getGuiScale().getValue() == 0 ? 2 : mc.options.getGuiScale().getValue());
         int columns = (int) Math.floor((float) (screenWidth / 2) / 105);
 
         int[] columnsY = new int[columns];
