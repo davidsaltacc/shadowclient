e1db01742a6d6d83baa12bf1712ddfa2ff8d9fc2
davidsaltacc <d.lumpi@outlook.de>
2024-09-05 11:01:30 +0200

    minor formatting

--- src/main/java/net/justacoder/shadowclient/mixin/GameOptionsMixin.java
+++ src/main/java/net/justacoder/shadowclient/mixin/GameOptionsMixin.java
@@ -11,15 +11,13 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import java.util.List;
 
 @Mixin(GameOptions.class)
 public abstract class GameOptionsMixin {
 
-    @Mutable
-    @Final
-    @Shadow
-    public KeyBinding[] allKeys;
+    @Mutable @Final @Shadow public KeyBinding[] allKeys;
 
     @Inject(method = "load", at = @At("HEAD"))
     public void onLoad(CallbackInfo info) {
