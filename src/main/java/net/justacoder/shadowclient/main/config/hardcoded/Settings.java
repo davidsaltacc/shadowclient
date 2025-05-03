4f0a99a70a6da094c0089060ab3d8afea3ec3af6
davidsaltacc <d.lumpi@outlook.de>
2025-05-03 13:43:22 +0200

    whoops

--- src/main/java/net/justacoder/shadowclient/main/config/hardcoded/Settings.java
+++ src/main/java/net/justacoder/shadowclient/main/config/hardcoded/Settings.java
@@ -1,7 +1,7 @@
 package net.justacoder.shadowclient.main.config.hardcoded;
 
-public class Settings { // just change these when building a jar, could automate this but cba
+public abstract class Settings { // just change these when building a jar, could automate this but cba
 
-    public static final boolean DEBUG_MODE = true;
+    public static final boolean DEBUG_MODE = false;
 
 }
